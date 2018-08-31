764b96920ebe25d9bc066b6d7f5afdaf31a5af59
赵东 <zhaodongxx@outlook.com>
2018-08-31 11:41:41 +0800

    重构 改变成多模块工程

--- chapter01/src/main/java/top/zhaodongxx/chapter01/Chapter01Application.java
+++ chapter01/src/main/java/top/zhaodongxx/chapter01/Chapter01Application.java
@@ -1,4 +1,4 @@
-package top.zhaodongxx;
+package top.zhaodongxx.chapter01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
